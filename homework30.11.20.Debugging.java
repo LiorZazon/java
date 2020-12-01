2ef0a537a201413a2509fc6442cae89b85b9539f
LiorZazon <75090709+LiorZazon@users.noreply.github.com>
2020-12-01 22:06:00 +0200

    Create homework30.11.20.Debugging.java

---
+++ homework30.11.20.Debugging.java
@@ -0,0 +1,4 @@
+args = {String[0]@815} 
+s = {Scanner@1011} "java.util.Scanner[delimiters=\p{javaWhitespace}+][position=5][match valid=true][need input=false][source closed=false][skipped=false][group separator=\x{2c}][decimal separator=\x{2e}][positive prefix=][negative prefix=\Q‎-\E][positive suffix=][negative suffix=][NaN string=\QNaN\E][infinity string=\Q∞\E]"
+my_name = "Bill Gates"
+my_savings = 125000
